82812bb5bb92427291736567adede5256f652e72
Arpitha93 <arpitha1993@gmail.com>
2022-05-20 16:17:24 -0700

    shopping App for clothing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.style.scss';
+import CategoryItem from './components/category-item/category-item.component';
 
 function App() {
+  const categories = [
+    {
+      "id": 1,
+      "title": 'Sweaters',
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+    },
+   { 
+     id: 6,
+      title: 'Girls',
+      imageUrl: 'https://www.gapfactory.com/Asset_Archive/GFWeb/content/0029/021/093/assets/GIRLS_DESKTOP_12.jpg?v=2'
+    },
+    {
+      id: 7,
+      title: 'Boys',
+      imageUrl: 'https://www.gapfactory.com/Asset_Archive/GFWeb/content/0029/021/096/assets/BOYS_DESKTOP_5.jpg?v=2'
+    },
+    {
+      id: 8,
+      title: 'Jeans',
+      imageUrl: 'https://www2.assets-gap.com/webcontent/0022/961/294/cn22961294.jpg'
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
